5aab758c1e8b4f5af0e4a6cd0bb0e910392a9e8c
akindursuun <1999akn@gmail.com>
2026-09-08 09:10:10 +0300

    Switch to static export for Plesk

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true
 }
 export default nextConfig
